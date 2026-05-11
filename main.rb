8fe13bb9b9c6bc408223089f635548d090004a09
lufy <tecfaisal8@gmail.com>
2026-05-11 17:20:01 +0000

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,35 +1,52 @@
 require './authvaultix.rb'
 
-AuthVaultix.new.Api(
+app = AuthVaultixClient.new(
     "", # Application Name
     "", # Application OwnerID
     "", # Application Secret
     "1.0" # Applicaiton Version
 )
 
-puts "\nConnecting..."
-AuthVaultix.new.Init
 
-puts "\n1) Login"
-puts "2) Register"
-puts "3) License Login"
-puts "4) Exit"
-print "\nChoose: "
-opt = gets.chomp
 
-case opt
-when '1'
-  print "Username: "; u = gets.chomp
-  print "Password: "; p = gets.chomp
-  AuthVaultix.new.Login(u, p)
-when '2'
-  print "Username: "; u = gets.chomp
-  print "Password: "; p = gets.chomp
-  print "License: "; k = gets.chomp
-  AuthVaultix.new.Register(u, p, k)
-when '3'
-  print "License: "; k = gets.chomp
-  AuthVaultix.new.License(k)
-else
-  puts "Goodbye!"
+puts "Connecting..."
+app.init
+
+loop do
+  puts "\n[1] Login"
+  puts "[2] Register"
+  puts "[3] License Login"
+  puts "[4] Upgrade"
+  puts "[5] Forgot Password"
+  puts "[6] Exit"
+  print "\nChoose option: "
+  opt = gets.chomp
+
+  case opt
+  when '1'
+    print "Username: "; u = gets.chomp
+    print "Password: "; p = gets.chomp
+    app.login(u, p)
+  when '2'
+    print "Username: "; u = gets.chomp
+    print "Password: "; p = gets.chomp
+    print "License: "; k = gets.chomp
+    app.register(u, p, k, "")
+  when '3'
+    print "License: "; k = gets.chomp
+    app.license_login(k)
+  when '4'
+    print "Username: "; u = gets.chomp
+    print "License: "; k = gets.chomp
+    app.upgrade(u, k)
+  when '5'
+    print "Username: "; u = gets.chomp
+    print "Email: "; e = gets.chomp
+    app.forgot_password(u, e)
+  when '6'
+    puts "Goodbye!"
+    break
+  else
+    puts "Invalid option!"
+  end
 end
